8caa89a82dd82be0a9675c5824cfe427701bf12b
vishnudk <vishnudk179@gmail.com>
2020-10-26 13:09:33 +0530

    both cpp and the python solution is not passing the first test case

--- b_ImplementAtoi/solutionCpp.cpp
+++ b_ImplementAtoi/solutionCpp.cpp
@@ -1,24 +1,33 @@
-// # include<iostream>
-// # include<conio.h> 
+# include<iostream>
+# include<conio.h> 
 
-// using namespace std;
-// int main()
-// {
-//     int t,l=0;
-//     cin >> t;
-//     string str;
-//     num = 0
-//     while(t--){
-//         cin>>str;
-//         for(int i=0;i<str.length();i++)
-//         {
-//             if(isdigit(str[i])){
-//              num = num * 10 +()     
-//             }
-//         }
-//     }
-//     return 0;
-// }
+using namespace std;
+int atoi(char str){
+    return str-'0';
+}
+int main()
+{
+    int t,l=0,num,flag = 1;
+    cin >> t;
+    string str;
+    while(t--){
+        flag = 1;
+        num = 0;
+        cin>>str;
+        for(int i=0;i<str.length();i++)
+        {
+            if(isdigit(str[i])){
+             num = num*10 + atoi(str[i]);
+            }
+            else if(str[i]=='-' || str[i] == '+'){
+                if(str[i] == '-')
+                    flag = -1;
+            }
+        }
+        cout<<num * flag <<endl;
+    }
+    return 0;
+}
 
 
 // the code is not completed the solution is completed in python
