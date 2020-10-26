bcb6f2dd37c70ac4c59ae0e5df3b7e3ba961c7be
vishnudk <vishnudk179@gmail.com>
2020-10-26 12:17:04 +0530

    added the solution to the qn super reduced string

--- superReduceString/solutionPython.py
+++ superReduceString/solutionPython.py
@@ -1,22 +1,19 @@
 s = list(input())
 l = len(s)
-i = 0
-uniq = s[:]
-while True:
-    l = len(s)
-    prv = len(uniq)
-    s.clear()
-    s = uniq[:]
-    uniq.clear()
-    while i < l:
-        if i+1<l and s[i]==s[i+1]:
-            i=i+2
+prv = -1
+while l!=prv:
+    if prv == -1:
+        prv = l
+    for i in range(len(s)-1):
+        try:
+            if s[i] == s[i+1]:
+                s.pop(i)
+                s.pop(i)
+        except:
             pass
-        else:
-            uniq.append(s[i])
-            i = i+1
-    if prv == len(uniq):
-        if len(uniq) == 0:
-            print("this string is invalid")
-        break
-print("".join(uniq))
+    prv = l
+    l = len(s)
+if len(s)!=0:
+    print("".join(s))
+else:
+    print("Empty String")
